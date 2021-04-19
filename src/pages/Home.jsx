e6fef52265be25719987320c015bdb0d26919129
julian.romana <julian.romana@getluko.com>
2021-04-19 18:17:28 +0200

    feat(map): add map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { useFetch } from '../hooks/fetch/useFetch'
 import { serializeUsers } from '../lib/serialize'
 import UsersList from '../components/Users/UsersList'
 import Loader from '../components/Loader'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 const Home = () => {
   const url = 'http://jsonplaceholder.typicode.com/users'
   const [users, loading, error] = useFetch(url)(serializeUsers)
 
   return (
-    <View style={styles.container}>
-      {!loading ? <UsersList users={users} /> : <Loader />}
-    </View>
+    <>
+      {!loading ? (
+        <View style={styles.container}>
+          <UsersList users={users} />
+          <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            region={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </View>
+      ) : (
+        <Loader />
+      )}
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: 300,
   },
 })
 
