78899aae9a15162d98519a80039d193ea94099e0
kentoje <k.monthubert@gmail.com>
2021-04-19 23:22:25 +0200

    feat(users): wip filter users

---
+++ src/hooks/filter/reducer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ENUM_TYPES } from './constants'
+
+const initialState = []
+
+const filterByFullName = (data, filter) =>
+  data.filter((item) =>
+    item.firstName.toLowerCase().includes(filter.toLowerCase())
+  )
+
+const reducer = (state = [], action) => {
+  if (action.type === ENUM_TYPES.filter) {
+    return filterByFullName(action.payload.data, action.payload.filter)
+  }
+
+  return state
+}
+
+export { initialState, reducer as filterReducer }
